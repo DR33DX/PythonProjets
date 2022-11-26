89d03ee25e8de8dc333cb0609765a2edef93dd4a
DR33DX <dr33dx13@gmail.com>
2022-11-26 13:21:53 -0400

    adicionado aul006b.py

---
+++ Aulas/aul006.py
@@ -0,0 +1,12 @@
+#Aula 006 sobre tipos primitivos
+#n1 e onde e armazenado valor 1 digitado 
+n1 = int(input('Digite um numero: '))
+#n2 e onde armazena valor 2 
+n2 = int(input('Digite outro numero: '))
+#s onde é armazenado valor da soma entre n1+n2
+s = n1+n2
+##comando print simples para imprimir na tela 
+print('A soma Vale',s)
+##comando print avançado com mais recurso
+print('A soma vale {}'.format(s))
+
