7753f465c7ab2f9de8ec976cbcf3ac5e0bad3f82
DR33DX <dr33dx13@gmail.com>
2022-11-27 08:44:37 -0400

    Adicionado comando {:.2f}

--- Exercicios/exe006.py
+++ Exercicios/exe006.py
@@ -1,6 +1,6 @@
-n = int(input('Digite um numero: '))
+n = float(input('Digite um numero: '))
 d = n * 2
 t = n * 3
-r = n ** 1/2
-print('O dobro de {} é {}\nO triplo é {}\nA raiz quadrada é {}'.format(n, d, t, r))
+r = n ** (1/2)
+print('O dobro de {} é {}\nO triplo é {}\nA raiz quadrada é {:.2f}'.format(n, d, t, r))
 
--- Exercicios/exe010.py
+++ Exercicios/exe010.py
@@ -1,4 +1,4 @@
 brl = float(input('Digite o valor a ser convertido: '))
 uss = 3.27
 cov = brl / uss
-print('{}R$ equivale a {}US$'.format(brl, cov))
+print('{}R$ equivale a {:.2f}US$'.format(brl, cov))
