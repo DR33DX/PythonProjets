a3c740620652de67af45ec38cea58cb35de25d84
DR33DX <dr33dx13@gmail.com>
2022-12-02 21:25:35 -0400

    atualização

---
+++ Aulas/aul009.py
@@ -0,0 +1,9 @@
+frase = 'Curso em Video Python'
+print(frase[13:1])
+print(frase[1:13:2])
+print(frase[1::3])
+print(frase[:12:2])
+print(frase.count('o'))
+print(frase.find('o'))
+print(len(frase))
+print(frase.split(''))
