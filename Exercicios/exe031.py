fce1442aae446771d05ca48a5d125e2095d583b3
DR33DX <dr33dx13@gmail.com>
2022-12-18 21:42:44 -0400

    atualizacao

--- Exercicios/exe031.py
+++ Exercicios/exe031.py
@@ -1,2 +1,11 @@
-n = input('long > ')
-print(n)
+dest = str(input('Nome do destino: ')).strip()
+orig = str(input('Nome da origem: ')).strip()
+p = float(input('Insira a kilometragen de sua viagem em KM: '))
+pkm = 0.5
+pkm2 = 0.45
+c = p * pkm
+c2 = p * pkm2
+if p>200:
+    print('Sua viagem de {} para {} custará {}'.format(orig, dest, c2))
+else:
+    print('Sua Viagem de {} para {} custará {}'.format(orig, dest, c))
